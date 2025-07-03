e3ff720970bb6746c63aafd3266f686c05152c6b
Shreyash A. Kamble <contactshreyashk@gmail.com>
2025-07-04 00:21:30 +0530

    INFO::Added Alert Functionality

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
-import { View, Text, TextInput, Pressable } from 'react-native';
+import { View, Text, TextInput, Pressable, Alert } from 'react-native';
 import './global.css';
+import { useState } from 'react';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    Alert.alert('Your Login Information', `Email: ${email}\nPassword: ${password}`, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'OK',
+        onPress: () => console.log('OK Pressed.'),
+      },
+    ]);
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-slate-100 px-4">
       <Text className="mb-4 text-3xl font-bold text-slate-900">Welcome!</Text>
@@ -9,15 +27,21 @@ export default function App() {
       <TextInput
         placeholder="Email"
         className="mb-3 h-12 w-full rounded-lg border border-slate-300 bg-white px-4"
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
         placeholder="Password"
         secureTextEntry
         className="mb-4 h-12 w-full rounded-lg border border-slate-300 bg-white px-4"
+        value={password}
+        onChangeText={setPassword}
       />
 
       <Pressable className="rounded-lg bg-blue-600 px-6 py-3">
-        <Text className="text-lg font-semibold text-white">Login</Text>
+        <Text className="text-lg font-semibold text-white" onPress={handleLogin}>
+          Login
+        </Text>
       </Pressable>
     </View>
   );
